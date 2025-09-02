a2a9815539fa6cf083ccab7938594269e9ec047f
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2025-09-02 17:47:54 +0200

    refactor: Passing props throught Button component

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -6,7 +6,10 @@ export function Home() {
   return (
     <View style={styles.container}>
       <Image style={styles.logo} source={require("@/assets/logo.png")} />
-      <Button />
+      <Button title="Criar conta" activeOpacity={0.6} onPress={() => console.log("Criar conta")}/>
+      <Button title="Adicionar" activeOpacity={0.5} onPress={() => console.log("Adicionar")}/>
+      <Button title="Salvar" activeOpacity={0.8} onPress={() => console.log("Salvar")}/>
+      <Button title="Voltar" activeOpacity={0.2} onPress={() => console.log("Voltar")}/>
     </View>
   )
 }
