106c3ac2523fdc1902b0426d7d21263156d0b7e2
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2025-09-03 17:45:59 +0200

    feat: Creating Input component

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,15 +1,14 @@
 import { View, Image } from "react-native";
 import { styles } from "./styles";
 import { Button } from "@/components/Button";
+import { Input } from "@/components/Input";
 
 export function Home() {
   return (
     <View style={styles.container}>
-      <Image style={styles.logo} source={require("@/assets/logo.png")} />
-      <Button title="Criar conta" activeOpacity={0.6} onPress={() => console.log("Criar conta")}/>
-      <Button title="Adicionar" activeOpacity={0.5} onPress={() => console.log("Adicionar")}/>
-      <Button title="Salvar" activeOpacity={0.8} onPress={() => console.log("Salvar")}/>
-      <Button title="Voltar" activeOpacity={0.2} onPress={() => console.log("Voltar")}/>
+      <Image source={require('@/assets/logo.png')} style={styles.logo}/>
+      <Input placeholder="¿Que necesitas comprar?" />
+      <Button title="Entrar"/>
     </View>
   )
 }
