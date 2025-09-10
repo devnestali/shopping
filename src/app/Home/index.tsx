af75c3547ccdc394e16c78df4f067a5b22fd4716
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2025-09-10 19:05:21 +0200

    feat: Creting states and functions to handle change filter, store description from input and store items

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,4 +1,5 @@
-import { View, Image, TouchableOpacity, Text, FlatList } from "react-native";
+import { useEffect, useState } from "react";
+import { View, Image, TouchableOpacity, Text, FlatList, Alert } from "react-native";
 import { styles } from "./styles";
 import { Button } from "@/components/Button";
 import { Input } from "@/components/Input";
@@ -8,47 +9,80 @@ import { FilterStatus } from "@/types/FilterStatus";
 import { Item } from "@/components/Item";
 
 const FILTER_STATUS: FilterStatus[] = [FilterStatus.PENDING, FilterStatus.DONE]
-const ITEMS = [
-  {
-    id: "1",
-    status: FilterStatus.DONE,
-    description: 'Comprar um pacote de café'
-  },
-  {
-    id: "2",
-    status: FilterStatus.PENDING,
-    description: 'Comprar três pacotes de macarrão'
-  },
-  {
-    id: "3",
-    status: FilterStatus.DONE,
-    description: 'Comprar três cebolas'
-  },
-]
 
-function handleRemoveItem() {
-  console.log('Remove item')
+interface ItemProps {
+  id: string
+  status: FilterStatus
+  description: string
 }
 
-function handleStatusChange() {
-  console.log('Cambia status')
-}
 
 export function Home() {
+  const [filter, setFilter] = useState<FilterStatus>()
+  const [description, setDescription] = useState<string>()
+  const [items, setItems] = useState<ItemProps[]>([])
+
+  function toggleFilter(status: FilterStatus) {
+    setFilter(status)
+  }
+
+  function handleInputText(value: string) {
+    setDescription(value)
+  }
+
+  function handleAddItem() {
+    if(!description?.trim()) {
+      return Alert.alert("Añadir", "Ingrese una descripción para agregar")
+    }
+
+    const newItem: ItemProps = {
+      id: Math.random().toString(36).substring(2),
+      description,
+      status: FilterStatus.PENDING
+    }
+
+    setItems((previousItems) => [newItem, ...previousItems ])
+  }
+
+  function handleRemoveItem() {
+    console.log('Remove item')
+  }
+
+  function handleStatusChange() {
+    console.log('Cambia status')
+  }
+
+  useEffect(() => {
+    console.log(items)
+  }, [items])
+  
   return (
     <View style={styles.container}>
       <Image source={require('@/assets/logo.png')} style={styles.logo}/>
       
       <View style={styles.form}>
-        <Input placeholder="¿Que necesitas comprar?" />
-        <Button title="Añadir"/>
+        <Input 
+          placeholder="¿Que necesitas comprar?"
+          onChangeText={(value) => handleInputText(value)}
+          />
+        <Button 
+          title="Añadir"
+          onPress={handleAddItem}
+        />
       </View>
 
       <View style={styles.content}>
         <View style={styles.header}>
           {
             FILTER_STATUS.map((status) => {
-              return <Filter key={status} status={status} isActive />
+              return (
+                <Filter 
+                  key={status} 
+                  status={status} 
+                  isActive={status === filter}
+                  onPress={() => toggleFilter(status)}
+                />
+              )
             })
           }
 
@@ -58,7 +92,7 @@ export function Home() {
         </View>
 
         <FlatList 
-          data={ITEMS}
+          data={items}
           keyExtractor={(item) => item.id}
           renderItem={({ item }) => {
             return (
@@ -66,7 +100,6 @@ export function Home() {
                 data={item}
                 onStatusChange={() => handleStatusChange()}
                 onRemove={() => handleRemoveItem()}
-                
               />
             )
           }}
